120698a0ff73c39b5c5e9e315d597812535dd9d5
Kleizjd <jose.jdgo98@gmail.com>
2022-09-16 20:35:54 -0500

    base datos

--- coffee_garden.sql
+++ coffee_garden.sql
@@ -26,38 +26,27 @@ CREATE TABLE IF NOT EXISTS `producto` (
   PRIMARY KEY (`codigo`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12345646 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla coffee_garden.producto: ~30 rows (aproximadamente)
+-- Volcando datos para la tabla coffee_garden.producto: ~21 rows (aproximadamente)
 INSERT INTO `producto` (`codigo`, `producto`, `categoria`, `precio`, `cantidad`, `estado`, `descripcion`) VALUES
-	(1, 'Nescafe', '1', 149000, 1000, 'A', ''),
-	(2, 'cafe aguila roja granulad', '1', 12000, 2000, 'A', ''),
-	(3, 'bolsa cafe sello rojo', '1', 51900, 1000, 'A', ''),
-	(4, 'vidrio cafe aroma', '1', 10000, 1000, 'A', ''),
-	(5, 'vidrio cafe legal descafe', '1', 15000, 2000, 'A', ''),
+	(1, 'Nescafe', '1', 149000, 2000, 'A', 'modifica'),
+	(2, 'cafe aguila roja granulad', '1', 12000, 2000, 'A', 'jose vamos'),
+	(3, 'bolsa cafe sello rojo', '1', 51900, 1000, 'A', 'desarrolla'),
+	(4, 'vidrio cafe aroma', '1', 10000, 1000, 'A', 'bastante'),
+	(5, 'vidrio cafe legal descafe', '1', 15000, 2000, 'A', 'yes very well'),
 	(7, 'vidrio cafe  dolca nescafe', '1', 12000, 1000, 'A', ''),
 	(8, 'vidrio cafe legal', '1', 10000, 1000, 'A', ''),
-	(9, 'Cafe soluble Nescafe Tasters c', '1', 175000, 1000, 'A', ''),
+	(9, 'Cafe soluble Nescafe Tasters c', '1', 175000, 1000, 'A', 'barato'),
 	(10, 'vidrio cafe colcate descafeina', '1', 175000, 1000, 'A', ''),
-	(11, 'plastico crama para cafe nestl', '1', 29900, 1000, 'A', ''),
+	(11, 'plastico crama para cafe nestle', '1', 29900, 1000, 'A', ''),
 	(12, 'carton crema para cafe vainill', '1', 30000, 1000, 'A', ''),
 	(13, 'crema para cafe aguila roja no', '1', 8000, 1000, 'A', ''),
 	(14, 'crema  para cafe aroma', '1', 6000, 1000, 'A', ''),
 	(15, 'plastico crema para cafe colcafe', '1', 15000, 1000, 'A', ''),
-	(16, '123', '', 0, 0, '', ''),
-	(123, '123', '', 0, 0, '', ''),
-	(456, 'carne', '', 0, 0, '', ''),
-	(886, '8', '', 0, 0, '', ''),
-	(3443, '434', '', 0, 0, '', ''),
-	(4534, '676', '', 0, 0, '', ''),
-	(8787, '8778', '', 0, 0, '', ''),
-	(9898, '9898', '', 0, 0, '', ''),
-	(12398, '123', '', 0, 0, '', ''),
-	(43534, '4534', '', 0, 0, '', ''),
-	(98999, '89', '', 0, 0, '', ''),
-	(123456, '123', '', 0, 0, '', ''),
-	(123777, '23', '', 0, 0, '', ''),
-	(123992, '123', '', 0, 0, '', ''),
-	(132423, 'asd', '1', 1000, 1000, 't', 'sad'),
-	(12345645, '123', '', 0, 0, '', '');
+	(123, '123', '', 0, 0, 'A', ''),
+	(456, 'carne', '', 0, 0, 'A', ''),
+	(886, '8', '', 0, 0, 'A', ''),
+	(3443, '434', '', 5, 5, 'A', '5'),
+	(12366, 'jugo de uva', '', 0, 0, '', '');
 
 -- Volcando estructura para tabla coffee_garden.proveedores
 CREATE TABLE IF NOT EXISTS `proveedores` (
@@ -73,9 +62,17 @@ INSERT INTO `proveedores` (`Nit_Proveedor`, `Nombre`) VALUES
 	('3', 'Procaps S.A'),
 	('4', 'Droguería Extra');
 
+-- Volcando estructura para tabla coffee_garden.rol_usuario
+CREATE TABLE IF NOT EXISTS `rol_usuario` (
+  `id` int(150) NOT NULL,
+  `rol` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Volcando datos para la tabla coffee_garden.rol_usuario: ~0 rows (aproximadamente)
+
 -- Volcando estructura para tabla coffee_garden.usuarios
 CREATE TABLE IF NOT EXISTS `usuarios` (
-  `id_usuario` varchar(15) NOT NULL,
+  `id_usuario` int(120) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(25) NOT NULL,
   `password` varchar(250) NOT NULL,
@@ -84,22 +81,27 @@ CREATE TABLE IF NOT EXISTS `usuarios` (
   `rolid` int(250) NOT NULL,
   `imagen_usuario` varchar(250) NOT NULL,
   PRIMARY KEY (`id_usuario`) USING BTREE
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla coffee_garden.usuarios: 11 rows
+-- Volcando datos para la tabla coffee_garden.usuarios: 15 rows
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
 INSERT INTO `usuarios` (`id_usuario`, `nombre`, `apellido`, `password`, `email`, `estado_usuario`, `rolid`, `imagen_usuario`) VALUES
-	('1107509521', 'José Daniel ', 'Grijalba', '1107509521', 'jose.jdgo97@gmail.com', 'A', 1, ''),
-	('1107509520', 'Juan David', 'Grijalba', '1107509520', 'juandgo1997@gmail.com', 'A', 2, ''),
-	('123', 'mario', ' 123', '123', '', 'A', 2, ''),
-	('diana', 'Diana', 'Aristizabal', '$2y$10$B4dfBodms5dKDvlRUBTnDeAJdlyzoukDFVzO/J7IpnmDtPBkXbv9q', 'dianaaristizabal@gmail.com', 'A', 2, 'diana-diana.png'),
-	('16', 'mario', 'hernandez', '$2y$10$Bjt2/1DXCYOQ9cWVvbJRnOuZ5bbFCjSUiZBFqDzgLJTMiJNMY8yAm', 'mario@gmail.com', 'A', 2, '2-16.jpg'),
-	('mario', 'mario', ' 123', '$2y$10$zOYYqMdOEzFAw1D4vz/jZ.EaEcB.3nsH8TzlU47oow9QI/SSp004e', '', 'A', 2, ''),
-	('89', 'Camilo', 'Delgado', '$2y$10$dmDf1V0Ib2b2bw1g7R3QtuGHnOEBQx14RkDS3bXHUy6tkJPDDGBr6', 'camilo@mail.com', 'A', 2, '6-89.jpg'),
-	('76', 'mario', ' 123', '$2y$10$ihy5RzyLszHudOUR7QzjOuM.3XAO2de9FJiZi1umVLQH9ZR5LaIrC', '', 'A', 2, ''),
-	('98', 'mario', ' 123', '$2y$10$Cp9Bi912X6jdEWavWT0M8uX0CUHeNPql7JUu4ZwL.r4MBcEyDnXEi', '', 'A', 2, ''),
-	('27', 'mario', ' 123', '$2y$10$KcGFFtuKnY6IMGVm4tGEEuZ3WsgvwBT/kJlSrrRJaYd9oR.MIUPMG', '', 'A', 2, ''),
-	('ioIO', 'mario', ' 123', '$2y$10$aH7rl.arBduDKsr9yPm4AeTX4RKBULIEal1c.UWlVVrfeKs2f5Wlu', '', 'A', 2, '');
+	(1, '   José Daniel', '   Grijalba', '$2y$10$An1ghzpGGYth9vnvA6CHveNqNWNqLtBDzMv.QOLhaIZkpeZsYR6HC', 'jose.jdgo97@gmail.com', 'A', 1, '2-1.jpg'),
+	(2, 'Juan David', 'Grijalba', '1107509520', 'juandgo1997@gmail.com', 'A', 2, ''),
+	(3, 'mario', ' 123', '123', '', 'A', 2, ''),
+	(4, '    Diana', '    Aristizabal', '$2y$10$TNQULbSAcW1Jojir7xbW/OKwAPUxAd3tE3Q.ePTsEzi.5qYFk2NxC', 'dianaaristizaba@gmail.com', 'A', 2, 'diana-diana-4.png'),
+	(5, 'mario', 'hernandez', '$2y$10$Bjt2/1DXCYOQ9cWVvbJRnOuZ5bbFCjSUiZBFqDzgLJTMiJNMY8yAm', 'mario@gmail.com', 'A', 2, '2-16.jpg'),
+	(6, 'mario', ' 123', '$2y$10$zOYYqMdOEzFAw1D4vz/jZ.EaEcB.3nsH8TzlU47oow9QI/SSp004e', '', 'A', 2, ''),
+	(7, 'Camilo', 'Delgado', '$2y$10$dmDf1V0Ib2b2bw1g7R3QtuGHnOEBQx14RkDS3bXHUy6tkJPDDGBr6', 'camilo@mail.com', 'A', 2, '6-89.jpg'),
+	(8, 'mario', ' 123', '$2y$10$ihy5RzyLszHudOUR7QzjOuM.3XAO2de9FJiZi1umVLQH9ZR5LaIrC', '', 'A', 2, ''),
+	(9, 'mario', ' 123', '$2y$10$Cp9Bi912X6jdEWavWT0M8uX0CUHeNPql7JUu4ZwL.r4MBcEyDnXEi', '', 'A', 2, ''),
+	(10, 'mario', ' 123', '$2y$10$KcGFFtuKnY6IMGVm4tGEEuZ3WsgvwBT/kJlSrrRJaYd9oR.MIUPMG', '', 'A', 2, ''),
+	(11, 'mario', ' 123', '$2y$10$aH7rl.arBduDKsr9yPm4AeTX4RKBULIEal1c.UWlVVrfeKs2f5Wlu', '', 'A', 2, ''),
+	(12, '1', '1', '$2y$10$ZfrQOY/34FR6XM31iWGVfOuYqquM7cCC71d8Uy7MwIsTP/GikFDJS', '', 'A', 2, ''),
+	(13, '12', '12', '$2y$10$Ze.pmyZP6Vdqx9Q55sk4T.UWE7nbIVTKru..n/zjXoDZvchitjAaC', '', 'A', 2, ''),
+	(14, '12', '12', '$2y$10$xFAAqAbQrRaEEjna1wi7X.HfOIiZJlsBh1NV5eKh1fBA2B8lZx7xe', '', 'A', 2, ''),
+	(15, 'manuel', 'grijalba', '$2y$10$Q36BTBWSKW0vFFEuJzIOtuv8LjA/Efc/xlBben6LD9qegXccLbDji', '', 'A', 2, ''),
+	(17, 'luis', 'garces', '$2y$10$mGIpInBVNSK3jeuSW23b3.iJuPvpRt259TWLKZYTKQ/lYKozA9/Ty', 'luis@mail.com', 'A', 2, '');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
