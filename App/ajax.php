c30481ecf44b53bc3bbc13189f75cc78330bc1c4
Kleizjd <jose.jdgo98@gmail.com>
2022-11-17 12:10:33 -0500

    3

--- App/ajax.php
+++ App/ajax.php
@@ -40,7 +40,7 @@ if (!empty($_POST)) {
     }
 } else {
     if(file_exists("views/home/listProducto.php")){
-        include_once "../Config/core.php";
+        include_once "../../Config/core.php";
 
         $producto= new Core();
         $sql = "SELECT * FROM producto";
