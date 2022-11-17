fca13254ae9ad2a22b50aee6446be9217022fbc4
Kleizjd <jose.jdgo98@gmail.com>
2022-11-17 17:37:43 -0500

    come on

--- views/home/home.php
+++ views/home/home.php
@@ -30,9 +30,11 @@ include_once "App/lib/Helpers.php"; ?>
 
 <body>
   <!-- Header -->
-  <div class="header"><!-- Navbar --><?php include_once "partials/navbar.php"; ?><!-- //Navbar --></div>
+  <div class="header">
+    <!-- Navbar --><?php include_once "partials/navbar.php"; ?><!-- //Navbar --></div>
   <!-- SIDE_BAR -->
-  <div id="cargarVista"><?php include_once "App/ajax.php"; ?></div>
+  <div id="cargarVista"><?php include_once "../login/login.php" ?></div>
+  <!-- <div id="cargarVista"><?php include_once "App/ajax.php"; ?></div> -->
   <!-- !SIDE_BAR -->
 </body>
 
